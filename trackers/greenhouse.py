457c63bf4e815693977ddcdc8abae7ad721f6a8e
AdwitaK <209945971+AdwitaK@users.noreply.github.com>
2026-08-27 15:50:44 -0400

    add job tracker for greenhouse ats

---
+++ trackers/greenhouse.py
@@ -0,0 +1,28 @@
+import requests
+from trackers.base_tracker import BaseTracker
+
+class GreenhouseTracker(BaseTracker):
+
+    def fetch_jobs(self, company):
+        identifier = company["identifier"]
+
+        url = f"https://boards-api.greenhouse.io/v1/boards/{identifier}/jobs"
+
+        response = requests.get(url)
+        response.raise_for_status()
+
+        data = response.json()
+
+        jobs = []
+
+        for job in data["jobs"]:
+            jobs.append({
+                "job_id": str(job["id"]),
+                "company": company["company"],
+                "title": job["title"],
+                "location": job["location"]["name"],
+                "url": job["absolute_url"]
+            })
+
+        return jobs
+
