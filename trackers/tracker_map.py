d7408ea71cc191ba22ee7495fe8c51e3964d0935
AdwitaK <209945971+AdwitaK@users.noreply.github.com>
2026-08-27 17:53:56 -0400

    update mapping with new ats systems

--- trackers/tracker_map.py
+++ trackers/tracker_map.py
@@ -2,11 +2,13 @@ from trackers.greenhouse import GreenhouseTracker
 from trackers.lever import LeverTracker
 from trackers.ashby import AshbyTracker
 from trackers.workable import WorkableTracker
+from trackers.workday import WorkdayTracker
 
 
 TRACKER_MAP = {
     "greenhouse": GreenhouseTracker, 
     "ashbyhq": AshbyTracker,
     "lever": LeverTracker, 
-    "workable": WorkableTracker
+    "workable": WorkableTracker,
+    "workday": WorkdayTracker
 }
