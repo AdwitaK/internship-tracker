4a77c9f051d533a68fd4f870e6f41f6d93164d47
AdwitaK <209945971+AdwitaK@users.noreply.github.com>
2026-08-28 14:09:41 -0400

    added error handling

--- main.py
+++ main.py
@@ -2,34 +2,69 @@ from core.company_loader import load_companies
 from trackers.tracker_map import TRACKER_MAP
 from core.seen_jobs import load_seen_jobs
 from core.seen_jobs import save_seen_jobs
+from core.job_filter import is_relevant
 
 def main():
     companies = load_companies()
     seen_jobs = load_seen_jobs()
     new_jobs = []
 
-    for company in companies[:1]:
+    for company in companies:
+
+        #Skip custom ats systems
         if(company["ats"] == "custom"):
             continue
 
+        # Instantiate correct ATS Tracker class
         tracker_class = TRACKER_MAP[company["ats"]]
+
+        ## Guard against missing ats
+        if tracker_class is None:
+            print(
+                f"Unknown ATS: {company['ats']} "
+                f"for {company['company']}"
+            )
+            continue
+
         tracker = tracker_class()
 
-        jobs = tracker.fetch_jobs(company)
+        # Fetch jobs from ATS, skip if error occurs
+        try:
+            jobs = tracker.fetch_jobs(company)
+        except Exception as e:
+            print(
+                f"Failed: {company['company']} "
+                f"({company['ats']})"
+            )
+            print(e)
+            continue
 
+        # Process fetched jobs
         for job in jobs:
+
+            ## skip malformed jobs
+            if not job["job_id"]:
+                continue
+
+            ## skip all jobs seen before
             if job["job_id"] in seen_jobs:
                 continue
 
-            new_jobs.append(job)
+            ## add newly found job to seen regardless of relevance
             seen_jobs[job["job_id"]] = {
                 "company": job["company"],
                 "title": job["title"]
             }
 
-        save_seen_jobs(seen_jobs)
+            ## check job relevance
+            if not is_relevant(job):
+                continue
 
-        print(f"Company: {company["company"]}, {len(new_jobs)} new jobs")
+            ## add to new jobs only if relevant
+            new_jobs.append(job)    
+
+    save_seen_jobs(seen_jobs)
+    print(f"{len(new_jobs)} new jobs found in {company["company"]}")
 
 
 if __name__ == "__main__":
