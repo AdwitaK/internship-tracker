b23531ce41b561eb00fc6834d50cd25a5a0a12bc
AdwitaK <209945971+AdwitaK@users.noreply.github.com>
2026-08-27 20:57:44 -0400

    add seen jobs logic

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from core.company_loader import load_companies
 from trackers.tracker_map import TRACKER_MAP
+from core.seen_jobs import load_seen_jobs
+from core.seen_jobs import save_seen_jobs
 
 def main():
     companies = load_companies()
+    seen_jobs = load_seen_jobs()
+    new_jobs = []
 
-    for company in companies[:10]:
+    for company in companies[:1]:
         if(company["ats"] == "custom"):
             continue
 
@@ -13,7 +17,19 @@ def main():
 
         jobs = tracker.fetch_jobs(company)
 
-        print(f"Company: {company["company"]}, {len(jobs)} jobs")
+        for job in jobs:
+            if job["job_id"] in seen_jobs:
+                continue
+
+            new_jobs.append(job)
+            seen_jobs[job["job_id"]] = {
+                "company": job["company"],
+                "title": job["title"]
+            }
+
+        save_seen_jobs(seen_jobs)
+
+        print(f"Company: {company["company"]}, {len(new_jobs)} new jobs")
 
 
 if __name__ == "__main__":
