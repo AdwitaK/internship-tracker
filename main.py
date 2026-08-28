0908d0e55f23b5c86d3c13ba17058c6e665bd2dd
AdwitaK <209945971+AdwitaK@users.noreply.github.com>
2026-08-27 20:38:47 -0400

    exclude custom from checks

--- main.py
+++ main.py
@@ -4,11 +4,10 @@ from trackers.tracker_map import TRACKER_MAP
 def main():
     companies = load_companies()
 
-    for company in companies:
-        #to be removed eventually
-        if(company["ats"] != "workable" and company["ats"] != "greenhouse" and company["ats"] != "ashbyhq" and company["ats"] != "lever"):
+    for company in companies[:10]:
+        if(company["ats"] == "custom"):
             continue
-        
+
         tracker_class = TRACKER_MAP[company["ats"]]
         tracker = tracker_class()
 
@@ -16,10 +15,6 @@ def main():
 
         print(f"Company: {company["company"]}, {len(jobs)} jobs")
 
-        '''for job in jobs[:5]:
-            print(f"  - {job['title']}")
-            print(f"    {job['location']}")
-            print(f"    {job['url']}")'''
 
 if __name__ == "__main__":
     main()
